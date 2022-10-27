e94c1dd4fb06cc0aa63acd5405557049779f7fb6
Davina Doran <davdoran@csu.fullerton.edu>
2022-10-27 11:42:02 -0700

    Update puzzle.py

--- puzzle.py
+++ puzzle.py
@@ -12,27 +12,36 @@ CLieosaurus = Symbol("C is a Lieosaurus")
 # Puzzle 0
 # A says "I am both a Truthoraptor and a Lieosaurus."
 knowledge0 = And(
+
+    # Info given bye the structure of the problem
     # A cannot be both a Truthoraptor and a Lieosaurus
     Biconditional(ATruthoraptor, Not(ALieosaurus)),
     Biconditional(ALieosaurus, Not(ATruthoraptor)),
-    
-    # Based in A's statement, If A is telling the truth (it is a truthoraptor), it is also a lieosauraus
-    Implication(ATruthoraptor, ALieosaurus)
+
+    # Info given bye the Dino's
+    # If A is Truthoraptor then A is a Lieosaurus
+    Implication(ATruthoraptor, ALieosaurus),
+
+    # If A is a Lieosaurus then A is not a Truthoraptor
+    Implication(ALieosaurus, Not(ATruthoraptor))
+
 )
 
 # Puzzle 1
 # A says "We are both Lieosauruss."
 # B says nothing.
 knowledge1 = And(
-    # Since A is a Lieosaurus then the statement "We are both Lieosauruss" cannot be true, therefore -(A^B)
 
-    # A dinosaur is cannot be both a Truthoraptor and a Lieosaurus
+    # Info given bye the structure of the problem
+    # A cannot be both a Truthoraptor and a Lieosaurus
     Biconditional(ATruthoraptor, Not(ALieosaurus)),
     Biconditional(ALieosaurus, Not(ATruthoraptor)),
+
+    # B cannot be both a Truthoraptor and a Lieosaurus
     Biconditional(BTruthoraptor, Not(BLieosaurus)),
     Biconditional(BLieosaurus, Not(BTruthoraptor)),
 
-
+    # Info given bye the Dino's
     # If A is a telling the truth, then both A and B are a Lieosauraus
     Implication(ATruthoraptor, And(ALieosaurus, BLieosaurus)),
 
@@ -44,19 +53,78 @@ knowledge1 = And(
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
-    # Since A is a Lieosaurus then they cannot be the same
-    Or(Symbol(ALieosaurus), Symbol(BLieosaurus)), 
-    Or(Symbol(ATruthoraptor), Symbol(BTruthoraptor))
+
+    # Info given byt the structure of the problem
+    # A cannot be both a Truthoraptor and a Lieosaurus
+    Biconditional(ATruthoraptor, Not(ALieosaurus)),
+    Biconditional(ALieosaurus, Not(ATruthoraptor)),
+
+    # B cannot be both a Truthoraptor and a Lieosaurus
+    Biconditional(BTruthoraptor, Not(BLieosaurus)),
+    Biconditional(BLieosaurus, Not(BTruthoraptor)),
+
+    # Info given bye the Dino's
+    # If A is a Truthoraptor then B is a Trutharaptor
+    Implication(ATruthoraptor, BTruthoraptor),
+
+    # If A is a Lieosaurus then B is Not a Lieosaurus
+    Implication(ALieosaurus, Not(BLieosaurus)),
+
+    # If B is a Truthoraptor then A is Not a Truthoraptor 
+    Implication(BTruthoraptor, Not(ATruthoraptor)),
+
+    # If B is a Lieosaurus then A is a Lieosaurus 
+    Implication(BLieosaurus, ALieosaurus)
 )
 
 # Puzzle 3
 # A says either "I am a Truthoraptor." or "I am a Lieosaurus.", but you don't know which.
-# B says "A said 'I am a Lieosaurus'." (If A is lie, and B cannot be the same as A, then B is truth, but this statement does not make sense with that logic?)
+# B says "A said 'I am a Lieosaurus'." 
 # B says "C is a Lieosaurus."
 # C says "A is a Truthoraptor."
 knowledge3 = And(
-    # TODO
+
+    # Info given bye the structure of the problem
+    # A cannot be both a Truthoraptor and a Lieosaurus
+    Biconditional(ATruthoraptor, Not(ALieosaurus)),
+    Biconditional(ALieosaurus, Not(ATruthoraptor)),
+
+    # B cannot be both a Truthoraptor and a Lieosaurus
+    Biconditional(BTruthoraptor, Not(BLieosaurus)),
+    Biconditional(BLieosaurus, Not(BTruthoraptor)),
+
+    # C cannot be both a Truthoraptor and a Lieosaurus
+    Biconditional(CTruthoraptor, Not(CLieosaurus)),
+    Biconditional(CLieosaurus, Not(CTruthoraptor)),
+
+
+    # Info given bye the Dino's
+    # A is either a Truthoraptor or I am a Lieosaurus, but not both
+    Or(ATruthoraptor, ALieosaurus),
+
+    # If B Truthoraptor and A Truthoraptor, then A is a Lieosaurus
+    Implication(And(BTruthoraptor, ATruthoraptor), ALieosaurus),
+
+    # If B Truthoraptor and A Lieosaurus, then A is Not a Lieosaurus
+    Implication(And(BTruthoraptor, ALieosaurus), Not(ALieosaurus)),
+
+    # If B Lieosaurus and A Truthoraptor, then A is Not a Lieosaurus
+    Implication(And(BLieosaurus, ATruthoraptor), Not(ALieosaurus)),
+
+    # If B Lieosaurus and A Lieosaurus, then A is a Lieosaurus
+    Implication(And(BLieosaurus, ALieosaurus), ALieosaurus),
+
+    # If B Truthoraptor then C is a Lieosaurus
+    Implication(BTruthoraptor, CLieosaurus),
+
+    # If B is Lieosaurus then C Not a Lieosaurus
+    Implication(BLieosaurus, Not(CLieosaurus)),
+
+    # If C Truthoraptor then A is a Truthoraptor
+    Implication(CTruthoraptor, ATruthoraptor),
+
+    # If C is Lieosaurus then C Not a Truthoraptor
+    Implication(CLieosaurus, Not(ATruthoraptor))
 )
 
 
